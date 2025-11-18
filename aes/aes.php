d032f09cd32345be24ab1e928f8457d46a33c207
zahra-create <zahirakasmouti@gmail.com>
2025-11-18 21:43:17 +0100

    aes improved

--- aes/aes.php
+++ aes/aes.php
@@ -1,9 +1,26 @@
 <?php
 $text = "your life and your experiences are the best resources
 you can learn from.";
-$cipher = "aes-192-cbc";
-$key = openssl_random_pseudo_bytes(8);
-$iv = openssl_random_pseudo_bytes(16);
-$ciphertext = openssl_encrypt($text, $cipher, $key, $options=0, $iv);
-$plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv);
+$cipher = "AES-192-CBC";
+$iv_length = openssl_cipher_iv_length($cipher);
+if (!$iv_length)
+{
+    throw new Exception("la méthode du chiffrement non définie!");
+}
+$passphrase = openssl_random_pseudo_bytes(8,$strong);
+if (!$strong)
+{ 
+    throw new Exception("la passphrase non sécurisée.");
+}
+$iv = openssl_random_pseudo_bytes($iv_length);
+$ciphertext = openssl_encrypt($text, $cipher, $passphrase, $options=0, $iv);
+if (!$ciphertext)
+{
+    throw new Exception("Echec du chiffrement !");
+}
+$plaintext = openssl_decrypt($ciphertext, $cipher, $passphrase, $options=0, $iv);
+if (!$plaintext)
+{
+    throw new Exception("Echec du déchiffrement !");
+}
 echo $plaintext . "\n";
